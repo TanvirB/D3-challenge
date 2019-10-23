81b839b2fe08e43a9dd9a541473242ddc43e0c1e
TanvirB <50238748+TanvirB@users.noreply.github.com>
2019-10-22 23:23:14 -0400

    fixed

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -2,10 +2,10 @@ var svgWidth = 960;
 var svgHeight = 500;
 
 var margin = {
-  top: 20,
-  right: 40,
-  bottom: 60,
-  left: 100
+    top: 20,
+    right: 40,
+    bottom: 60,
+    left: 100
 };
 
 var width = svgWidth - margin.left - margin.right;
@@ -13,38 +13,41 @@ var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
 var svg = d3.select(".chart")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
 
 var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data
-d3.csv("assets/data/data.csv")
-  .then(function(stateData) {
+d3.csv("assets/data/data.csv", function (err, stateData) {
+    if (err) throw err;
 
-    stateData.forEach(function(data) {
+    stateData.forEach(function (data) {
         data.smokes = +data.smokes;
         data.age = +data.age;
     });
 
     //Create Scales
     var xLinearScale = d3.scaleLinear()
-        .domain([20, d3.max(stateData, d=> d.smokes)])
+        .domain([d3.min(stateData, d => d.smokes) - 2, d3.max(stateData, d => d.smokes)])
         .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-        .domain([0, d3.max(stateData, d => d.age)])
-        .range([height,0]);
+        .domain([d3.min(stateData, d => d.age) - 2, d3.max(stateData, d => d.age)])
+        .range([height, 0]);
 
     //create axis functions + append to chart
     var bottomAxis = d3.axisBottom(xLinearScale);
     var leftAxis = d3.axisLeft(yLinearScale);
 
     chartGroup.append("g")
-        .attr("transform",`translate(0, ${height})`)
+        .attr("transform", `translate(0, ${height})`)
         .call(bottomAxis);
+
+    chartGroup.append("g")
+        .call(leftAxis);
 
     //Create the Scatter circles
     var circlesGroup = chartGroup.selectAll("circle")
@@ -53,40 +56,45 @@ d3.csv("assets/data/data.csv")
         .append("circle")
         .attr("cx", d => xLinearScale(d.smokes))
         .attr("cy", d => yLinearScale(d.age))
-        .attr("r","15")
-        .attr("fill","orange")
-        .attr("opacity","0.5");
-    
+        .attr("r", "15")
+        .attr("fill", "orange")
+        .attr("opacity", "0.5");
+
+    //Axis Labels
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 40)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Smoking Rate %");
+
+    chartGroup.append("text")
+        .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+        .attr("class", "axisText")
+        .text("Age in Years");
+
     //Tooltip
     var toolTip = d3.tip()
-        .attr("class","tooltip")
+        .attr("class", "tooltip")
         .offset([80, -60])
         .html(function (d) {
-            return(`${d.state}<br>Smokers: ${d.smokes}<br>Age: ${d.age}`);
+            return (`${d.state}<br>Smokers: ${d.smokes}<br>Age: ${d.age}`);
         });
 
     //tooltip in chart
     chartGroup.call(toolTip);
 
     //Listeners
-    circlesGroup.on("click", function(data) {
-        toolTip.show(data,this);
+    circlesGroup.on("click", function (data) {
+        toolTip.show(data, this);
     })
-        ,on("mouseout", function(data, index){
+        , on("mouseout", function (data, index) {
             toolTip.hide(data);
         })
-    
-    //Axis Labels
-    chartGroup.append("text")
-        .attr("transform", "rotate(-90")
-        .attr("y", 0 - margin.left + 40)
-        .attr("x", 0 - (height/2))
-        .attr("dy", "1em")
-        .attr("class", "axisText")
-        .text("Smoking Rate %");
 
-        chartGroup.append("text")
-        .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-        .attr("class", "axisText")
-        .text("Age in Years");
-    });
+
+
+});
+
+
